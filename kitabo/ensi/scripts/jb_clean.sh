ad6ab9bb02f432479d9ff685a2942cc1b87d452a
abikesa <abikesa.sh@gmail.com>
2024-08-21 17:17:16 -0400

    music now has breathtaking nodes & wedges

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,9 +1,8 @@
-# transfer after every jb clean .
-mkdir -p _build/html/pdfs  
-cp pdfs/locke.pdf _build/html/pdfs/locke.pdf
-cp pdfs/bach.pdf _build/html/pdfs/bach.pdf
-cp pdfs/handel.pdf _build/html/pdfs/handel.pdf
-cp pdfs/mozart.pdf _build/html/pdfs/mozart.pdf
-mkdir -p _build/html/media
-cp media/jesus-loves-me.mp3 _build/html/media/jesus-loves-me.mp3
+mkdir -p _build/html/pdfs _build/html/figures _build/html/media
+cp -r figures/* _build/html/figures/
+cp -r pdfs/* _build/html/pdfs/
+cp -r media/* _build/html/media/
 cp -r act1/app/* _build/html/act1/app
+
+
+
